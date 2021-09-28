c7ff1260186bf02c4c337785c93eabb284c31299
compaeasy <compaeasy@yandex.ru>
2021-09-28 21:36:14 +0300

    Внес правки по 1 и 2 заданию

--- js/main.js
+++ js/main.js
@@ -1,23 +1,21 @@
-function randomNumber(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// eslint-disable-next-line no-unused-vars
+const getRandomInt = (min, max) => {
+  if (min < 0) {
+    throw new Error('Min не должен быть меньше 0');
+  }
+  if (max < 0) {
+    throw new Error('Max не должен быть меньше 0');
+  }
+  if (max <= min) {
+    throw new Error('Mim должен быть меньше max');
+  }
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-// eslint-disable-next-line no-console
-console.log(randomNumber(0, 15));
+getRandomInt(2, 5);
 
 
 // eslint-disable-next-line no-unused-vars
-const maxStringValue = function (name) {
-  if (name.length < 6) {
-    // eslint-disable-next-line no-console
-    console.log(' Покупает шоколадные конфеты');
-    return true;
-  } else {
-    // eslint-disable-next-line no-console
-    console.log(' Не покупает шоколадные конфеты');
-    return false;
-  }
-};
+const checkMaxStringLength = (value, maxLength) => value.length > maxLength;
 
-maxStringValue('Анастасия');
-
+checkMaxStringLength('Каждый охотник желает знать где сидит фазан', 140);
